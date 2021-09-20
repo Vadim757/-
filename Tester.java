9aed53f4fe5d167bc58d716abdc59c2aeba8b401
Vadim757 <90616980+Vadim757@users.noreply.github.com>
2021-09-20 11:06:30 +0300

    Delete Tester.java

--- Tester.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-import java.lang.*;
-
-public class Tester {
-    public static void main(String[] args) {
-        Book book = new Book("Oscar Wilde", 1890, 320);
-        System.out.println(book);
-    }
-}
