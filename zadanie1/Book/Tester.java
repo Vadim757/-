22b0653d2155183586c9876974c16a9f0fb055b0
Vadim757 <vadim122002@mail.ru>
2021-09-20 11:13:14 +0300

    Задание1

---
+++ zadanie1/Book/Tester.java
@@ -0,0 +1,9 @@
+package com.company;
+import java.lang.*;
+
+public class Tester {
+    public static void main(String[] args) {
+        Book book = new Book("Oscar Wilde", 1890, 320);
+        System.out.println(book);
+    }
+}
