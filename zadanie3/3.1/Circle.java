7e21331dc0f7e6951c8f3f0cec29def921b2ee44
Vadim757 <90616980+Vadim757@users.noreply.github.com>
2021-09-26 14:26:49 +0300

    Add files via upload

---
+++ zadanie3/3.1/Circle.java
@@ -0,0 +1,48 @@
+package com.company;
+
+public class Circle extends Shape {
+
+    protected double radius;
+
+    public Circle(String color, boolean filled, double radius) {
+        super(color, filled);
+        this.radius = radius;
+    }
+
+    public Circle(double radius) {
+        super();
+        this.radius = radius;
+    }
+
+    public Circle() {
+        super();
+        radius = 0;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                ", colour='" + color + '\'' +
+                ", filled=" + filled +
+                '}';
+    }
+
+    @Override
+    public double getPerimeter() {
+        return Math.PI * radius * 2;
+    }
+}
